f8f003cd29b387aaf75ed3a0353d0dfe90b9348c
marsya505 <marsyazulfazahrani0@gmail.com>
2024-11-02 14:19:01 +0700

    commit & push assignment

--- AssignmentTicketCinema11.java
+++ AssignmentTicketCinema11.java
@@ -4,12 +4,9 @@ public class AssignmentTicketCinema11 {
         int ticketPrice = 50000, tickets, totalTicketsSold = 0;
         double totalSales = 0.0, discount = 0.0, totalPrice;
         Scanner input11 = new Scanner(System.in);
-        System.out.print("How much the tickets sold: ");
-        tickets = input11.nextInt();
         while (true) {
-            if (tickets == -1) {
-                break;
-            }
+            System.out.print("How much the tickets sold: ");
+            tickets = input11.nextInt();
             if (tickets < 0) {
                 System.out.println("Invalid");
                 continue; 
@@ -22,7 +19,7 @@ public class AssignmentTicketCinema11 {
             totalPrice = tickets * ticketPrice * (1 - discount);
             totalTicketsSold += tickets;
             totalSales += totalPrice;
-            break;
+            break; 
         }
         System.out.printf("%nTotal tickets sold: %d%n", totalTicketsSold);
         System.out.printf("Total ticket sales: Rp %.2f%n", totalSales);
